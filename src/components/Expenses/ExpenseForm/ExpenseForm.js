a59581b1653fdfa95327d23e20fa1cbe1ec80238
kamalyadav <kamal@gomechanic.in>
2021-05-02 17:51:10 +0530

    code changes

--- src/components/Expenses/ExpenseForm/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm/ExpenseForm.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = ({ addNewExpense }) => {
+const ExpenseForm = ({ addNewExpense, setShowAddButton, toggleAddButton }) => {
 	const [formData, setFormData] = useState({
 		title: "",
 		amount: "",
 		date: "",
 	});
-	const [showAddButton, setShowAddButton] = useState(true);
 
 	const onChangeHandler = (e) => {
 		const { name, value } = e.target;
@@ -19,61 +18,52 @@ const ExpenseForm = ({ addNewExpense }) => {
 	const onSubmitHandler = (e) => {
 		e.preventDefault();
 		addNewExpense(formData);
-		setShowAddButton(true);
-		setFormData({ title: "", amount: "", date: "" });
-	};
-
-	const toggleAddButton = () => {
-		setShowAddButton(!showAddButton);
+		toggleAddButton();
 	};
 
 	return (
 		<>
-			{showAddButton ? (
-				<div className="new-expense__add">
-					<button onClick={toggleAddButton}>Add New Expense</button>
+			<form onSubmit={onSubmitHandler} className="expense-form">
+				<div className="new-expense__controls">
+					<div className="new-expense__control">
+						<label>Title</label>
+						<input
+							name="title"
+							value={formData.title}
+							type="text"
+							onChange={onChangeHandler}
+						/>
+					</div>
+					<div className="new-expense__control">
+						<label>Amount</label>
+						<input
+							name="amount"
+							value={formData.amount}
+							type="number"
+							onChange={onChangeHandler}
+						/>
+					</div>
+					<div className="new-expense__control">
+						<label>Date</label>
+						<input
+							name="date"
+							value={formData.date}
+							type="date"
+							onChange={onChangeHandler}
+						/>
+					</div>
 				</div>
-			) : (
-				<form onSubmit={onSubmitHandler} className="expense-form">
-					<div className="new-expense__controls">
-						<div className="new-expense__control">
-							<label>Title</label>
-							<input
-								name="title"
-								value={formData.title}
-								type="text"
-								onChange={onChangeHandler}
-							/>
-						</div>
-						<div className="new-expense__control">
-							<label>Amount</label>
-							<input
-								name="amount"
-								value={formData.amount}
-								type="number"
-								onChange={onChangeHandler}
-							/>
-						</div>
-						<div className="new-expense__control">
-							<label>Date</label>
-							<input
-								name="date"
-								value={formData.date}
-								type="date"
-								onChange={onChangeHandler}
-							/>
-						</div>
+				<div className="new-expense__actions">
+					<div>
+						<button type="button" onClick={toggleAddButton}>
+							Cancel
+						</button>
 					</div>
-					<div className="new-expense__actions">
-						<div>
-							<button onClick={toggleAddButton}>Cancel</button>
-						</div>
-						<div>
-							<button type="submit">Add Expense</button>
-						</div>
+					<div>
+						<button type="submit">Add Expense</button>
 					</div>
-				</form>
-			)}
+				</div>
+			</form>
 		</>
 	);
 };
--- src/components/Expenses/NewExpense/NewExpense.js
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = ({ addNewExpense }) => {
+	const [showAddButton, setShowAddButton] = useState(true);
+
+	const toggleAddButton = () => {
+		setShowAddButton(!showAddButton);
+	};
+
 	return (
 		<div className="new-expense">
-			<ExpenseForm addNewExpense={addNewExpense} />
+			{showAddButton ? (
+				<div className="new-expense__add">
+					<button onClick={toggleAddButton}>Add New Expense</button>
+				</div>
+			) : (
+				<ExpenseForm
+					addNewExpense={addNewExpense}
+					toggleAddButton={toggleAddButton}
+				/>
+			)}
 		</div>
 	);
 };
